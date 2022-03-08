37e071973a17978b38c532acc1d3f8cc2168adb8
add new form testdrive <stinex.sx@gmail.com>
2022-03-08 18:16:52 +0500

    08.03

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -10,53 +10,35 @@ export const HomePage = () => {
 
 
     const Model = () => {
-        const {scene} = useGLTF('/104_BODY_04_PBR.gltf')
+        const { scene } = useGLTF('/104_BODY_06_PBR.gltf')
         return <primitive object={scene} />
     }
-
-
-
-    /*   function Model() {
-        let fbx = useFBX('/104_BODY_01.fbx')
-        return <primitive object={fbx} dispose={null} />
-      } */
-
-
-
 
     const Lights = () => {
         return (
             <>
                 <ambientLight intensity={0.4} />
-                <directionalLight position={[0, 10, 0]} intensity={1.4} />
-                <directionalLight position={[10, 10, 0]} intensity={1.5} />
+                <directionalLight position={[0, 10, 0]} intensity={1} />
+                <directionalLight position={[10, 10, 0]} intensity={1} />
             </>
         )
     }
 
     const HTMLcontent = () => {
         const texture_1 = useLoader(TextureLoader, '/textur.png');
-        // const texture = useTexture('/textur.png')
 
 
         return (
-            <>
-                <mesh position={[0, -90, 0]}>
-                    <Model />
-                    <meshStandardMaterial attach="material" map={texture_1}  />
-                </mesh>
-                <Html fullscreen>
-                    <div className="main">
-                        123
-                    </div>
-                </Html>
-            </>
+            <mesh position={[0, -90, 0]}>
+                <Model />
+                <meshStandardMaterial attach="material" map={texture_1} />
+            </mesh>
         )
     }
     return (
         <div className='container__wrapper'>
             <div className="home">
-                <div className="home__wrapper">
+                <div className="home__wrapper ">
                     <div className="nav__home">
                         <a href="/tracks-page" ><span></span>треки</a>
                         <a href="/video-page" ><span></span>видео</a>
@@ -64,15 +46,20 @@ export const HomePage = () => {
                         <a href="/contacts-page" ><span></span>контакты</a>
                         <a href="/concerts-page" ><span></span>концерты</a>
                     </div>
-                    <Suspense fallback={null}>
-                        <Canvas
-                            colorManagement
-                            camera={{ position: [0, 40, -440, 0], fov: 30 }}
-                        >
-                            <Lights />
-                            <HTMLcontent />
-                        </Canvas>
-                    </Suspense>
+
+                    <div className="block">
+                        <div className="canvas">
+                            <Suspense fallback={null}>
+                                <Canvas
+                                    colorManagement
+                                    camera={{ position: [0, 40, -440, 0], fov: 30 }}
+                                >
+                                    <Lights />
+                                    <HTMLcontent />
+                                </Canvas>
+                            </Suspense>
+                        </div>
+                    </div>
                     <div className="information">
                         <span>// музыкант</span>
                         <span>рэп-исполнитель</span>
